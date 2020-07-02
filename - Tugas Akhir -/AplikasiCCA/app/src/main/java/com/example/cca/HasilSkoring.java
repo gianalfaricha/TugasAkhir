d6b7c52bfd3703e5d3e6a58d8fcf04fc8b8a61e2
Gian Alfaricha <gianalfaricha@gmail.com>
2020-07-02 20:00:43 +0700

    Aplikasi Cerdas Cermat Agama Islam

---
+++ - Tugas Akhir -/AplikasiCCA/app/src/main/java/com/example/cca/HasilSkoring.java
@@ -0,0 +1,49 @@
+package com.example.cca;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HasilSkoring extends AppCompatActivity {
+    TextView mtvHasilAkhir;
+    Button mbtnMenu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hasil_skoring);
+
+        mtvHasilAkhir = (TextView) findViewById(R.id.tvSkorAkhir);
+        mbtnMenu = (Button) findViewById(R.id.btnMenu);
+
+        setSkor();
+
+        mbtnMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(HasilSkoring.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+    public void setSkor(){
+        String activity = getIntent().getStringExtra("activity");
+        String skorPilGan = getIntent().getStringExtra("skorAkhir");
+        String skorEssay = getIntent().getStringExtra("skorAkhir2");
+
+        if(activity.equals("PilihanGanda")){
+            mtvHasilAkhir.setText("SKOR : "+skorPilGan);
+        }else{
+            mtvHasilAkhir.setText("SKOR : "+skorEssay);
+        }
+    }
+
+    public void onBackPressed(){
+        Toast.makeText(this, "Tidak bisa kembali, silahkan tekan menu", Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ - Tugas Akhir -/AplikasiCCAislam/app/src/main/java/com/example/aplikasicca/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.aplikasicca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+    LinearLayout mlinePilGan, mLineEssay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mlinePilGan = (LinearLayout) findViewById(R.id.linePilGan);
+        mLineEssay = (LinearLayout) findViewById(R.id.lineEssay);
+
+        mlinePilGan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, KuisPilihanGanda.class);
+                startActivity(i);
+            }
+        });
+
+        mLineEssay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, KuisEssay.class);
+                startActivity(i);
+            }
+        });
+    }
+}
